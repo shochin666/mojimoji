2f4d73e4c4936e84b355886257b30d78df79bfc9
shochin666 <shoogawa111@gmail.com>
2023-02-23 18:29:41 +0900

    アナリティクスを導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { Top } from "./components/Top";
 import "./App.css";
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+const firebaseConfig = {
+  apiKey: "AIzaSyDfP_MaILbm25ABT4-6dkuKdQ2r5r6NwCI",
+  authDomain: "mojimoji-f7149.firebaseapp.com",
+  projectId: "mojimoji-f7149",
+  storageBucket: "mojimoji-f7149.appspot.com",
+  messagingSenderId: "1084590490023",
+  appId: "1:1084590490023:web:77ce44716ee4328ec3f682",
+  measurementId: "G-ST8C4B1KED",
+};
+
 function App() {
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
   return (
     <BrowserRouter>
       <Routes>
